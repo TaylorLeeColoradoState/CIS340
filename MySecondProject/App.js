d89557286fb8a943be20f16fdb83381ee1d5aa63
TaylorLeeColoradoState <103369961+TaylorLeeColoradoState@users.noreply.github.com>
2022-04-15 18:19:11 +0000

    Create App.js

---
+++ MySecondProject/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text,TextInput, View } from 'react-native';
+
+ function MyApp() {
+  return (
+    <View>
+      <Text>
+        Hello I am a student in CIS340!
+      </Text>
+    </View>
+
+  );
+}
+
+export default function MultiComp()
+{ 
+  
+  return(
+    <View stye = {{
+      flex: 1,
+      justifyContent: "Center",
+      alignItems: "center",
+    }}>
+      <Text> Welcome to my class </Text>
+      <MyApp />
+      <MyApp />
+      <MyApp />
+      <MyApp />
+
+    </View>
+
+
+  )
+}
