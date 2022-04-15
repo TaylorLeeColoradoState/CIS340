0fb8afe0ffa1deb1acbc9f6b6731e25bd7938ed4
TaylorLeeColoradoState <103369961+TaylorLeeColoradoState@users.noreply.github.com>
2022-04-15 18:15:46 +0000

    Create App copy.js

---
+++ MySecondProject/assets/App copy.js
@@ -0,0 +1,29 @@
+@ -0,0 +1,29 @@
+import { React } from 'react';
+import { Text,TextInput, View } from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName(fname,mname,lname)
+  {
+    return fname + " " + mname + " " + lname;
+  }
+
+const pet = "Dog";
+  return (
+<view>
+      <Text>
+        {"\n\n\n\n\n\n"}
+        Hello I am a student in CIS340!
+
+        </Text>
+        <TextInput 
+        style = { {
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        defaultValue = "Enter Input"/>
+</view>
+  );
+}
