4a1fb5ae72d0eff22f92c7d0fcf492e7fc1ad74f
TaylorLeeColoradoState <103369961+TaylorLeeColoradoState@users.noreply.github.com>
2022-04-21 15:27:32 +0000

    Create app.copy.js

---
+++ MyFourthProject/app.copy.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+import {Text, View } from 'react-native';
+
+
+export default class MyApp extends Component  {
+  trnder(){
+  return (
+    <View style={{padding: 60}}>
+      <Text>Hello I am here, I am a studfent in CIS340</Text>
+      <Text>I like using the class component</Text>
+    </View>
+  );
+}
+}
