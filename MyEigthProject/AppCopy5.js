ad087224ea26ff5be11e96b8031b6e1880d48c4b
TaylorLeeColoradoState <103369961+TaylorLeeColoradoState@users.noreply.github.com>
2022-05-02 19:23:47 +0000

    Create AppCopy5.js

---
+++ MyEigthProject/AppCopy5.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableObject, image } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+
+
+export default function App() {
+
+const[slectedImage, setSelectedImage] = React.useState(null)
+
+let openImagePickerAsync = async () =>{
+ 
+  let permissionResult = await openImagePickerAsync.requestCameraRollPermissionsAsync();
+
+  if(permissionResult.granted === false){
+    alert('Permission is required');
+    return;
+  }
+
+
+  let pickerResult = await ImagePicker.launchImageLibraryasync();
+  
+  if(pickerResult.Cancelled ===true){
+    return;
+  }
+  setSelectedImage({localURL: pickerResult.url});
+
+}
+
+
+
+let openSharingDialogAsync = async() => {
+  if(!(await openSharingDialogAsync.isAvailableAsync())){
+    alert('Sharing is not available on my phone')
+    return;
+
+  }
+  openSharingDialogAsync.shareAsync(selectedImage.localURi);
+};
+
+
+if(selectedImage !== null){
+  return(
+    <View style = {styles.container}>
+    <Image source = {{ url: selectedImage.localURL}} style ={styles.selImage}/>
+    <TouchableOpacity onPress = {opensharingDialogAsync} style = {styles.selImage}>
+      <Text>style ={styles.buttonText} share My Photo </Text>
+    </TouchableOpacity>
+    </View>
+  )
+}
+
+  return (
+    <View style={styles.container}>
+       <image source = {{url: ''}}
+       style = {styles.logo}/>
+
+      <Text style = {styles.inits}> Press the button below to select images on your phone</Text>
+   
+      <TouchableOpacity style = {style.container} onPress = {() => alert('you have not slected an image yet')}>
+        
+        <Text style= {styles.buttonText} > pick image </Text>
+
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#FFFFE0'
+  },
+  button: {
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: '#FFFFE0'
+  },
+  insts:{
+    fontSize: 18,
+    color: '#FFFFE0',
+    marginHorizontal: 15,
+    marginBottom: 20
+  },
+  logo:{
+    width: 310,
+    height: 300,
+    marginBottom: 20
+
+  },
+  buttonText:{
+    fontSize: 20,
+    color: '#FFFFE0'
+  }
+
+});
